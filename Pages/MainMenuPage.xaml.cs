8b22237f8fa42b2e5277946efb2d5c0040ce8ea1
danderle <danderle86@gmail.com>
2022-10-04 23:21:39 +0200

    Add project files.

---
+++ Pages/MainMenuPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace Battleships;
+
+/// <summary>
+/// Interaction logic for MainMenuPage.xaml
+/// </summary>
+public partial class MainMenuPage : Page
+{
+    public MainMenuPage()
+    {
+        InitializeComponent();
+    }
+}
---
+++ ViewModels/MainMenuViewModel.cs
@@ -0,0 +1,11 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Battleships;
+
+internal partial class MainMenuViewModel : ObservableObject
+{
+	public MainMenuViewModel()
+	{
+
+	}
+}
